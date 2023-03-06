1801b2ea6916d6c4cd7c1211c9c64657b3e07627
Evgeny Eremin <ereminevgeny78@gmail.com>
2023-03-06 19:50:44 +0700

    change logic, change styles

--- src/components/TableData/TableData.tsx
+++ src/components/TableData/TableData.tsx
@@ -38,47 +38,58 @@ const TableData = (props: any) => {
     }
   };
 
-  const setPage1 = () => {
-    setInitPage(0);
-    setEndPage(10);
-  };
-  const setPage2 = () => {
-    setInitPage(10);
-    setEndPage(20);
-  };
-  const setPage3 = () => {
-    setInitPage(20);
-    setEndPage(30);
-  };
-  const setPage4 = () => {
-    setInitPage(30);
-    setEndPage(40);
-  };
-  const setPage5 = () => {
-    setInitPage(40);
-    setEndPage(50);
-  };
-  const setPage6 = () => {
-    setInitPage(50);
-    setEndPage(60);
-  };
-  const setPage7 = () => {
-    setInitPage(60);
-    setEndPage(70);
-  };
-  const setPage8 = () => {
-    setInitPage(70);
-    setEndPage(80);
-  };
-  const setPage9 = () => {
-    setInitPage(80);
-    setEndPage(90);
-  };
-  const setPage10 = () => {
-    setInitPage(90);
-    setEndPage(100);
-  };
+  const arrOfPageButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const pageButtons = arrOfPageButtons.map((button: any) => {
+    return (
+      <button className="list-buttons"
+        onClick={() => {
+          if (button === 1) {
+            setInitPage(0);
+            setEndPage(10);
+          }
+          if (button === 2) {
+            setInitPage(10);
+            setEndPage(20);
+          }
+          if (button === 3) {
+            setInitPage(20);
+            setEndPage(30);
+          }
+          if (button === 4) {
+            setInitPage(30);
+            setEndPage(40);
+          }
+          if (button === 5) {
+            setInitPage(40);
+            setEndPage(50);
+          }
 
+          if (button === 6) {
+            setInitPage(50);
+            setEndPage(60);
+          }
+          if (button === 7) {
+            setInitPage(60);
+            setEndPage(70);
+          }
+          if (button === 8) {
+            setInitPage(70);
+            setEndPage(80);
+          }
+          if (button === 9) {
+            setInitPage(80);
+            setEndPage(90);
+          }
+          if (button === 10) {
+            setInitPage(90);
+            setEndPage(100);
+          }
+        }}
+      >
+        {button}
+      </button>
+    );
+  });
   // const arrOfId = elemInfo.map((item: any) => {
   //   return item.id;
   // });
@@ -114,18 +125,7 @@ const TableData = (props: any) => {
           Назад
         </button>
 
-        <ul>
-          <li onClick={setPage1}>1</li>
-          <li onClick={setPage2}>2</li>
-          <li onClick={setPage3}>3</li>
-          <li onClick={setPage4}>4</li>
-          <li onClick={setPage5}>5</li>
-          <li onClick={setPage6}>6</li>
-          <li onClick={setPage7}>7</li>
-          <li onClick={setPage8}>8</li>
-          <li onClick={setPage9}>9</li>
-          <li onClick={setPage10}>10</li>
-        </ul>
+        <div>{pageButtons}</div>
 
         <button className="change-page__button" onClick={showNextInfo}>
           Далее
